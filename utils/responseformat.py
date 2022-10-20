a752dfc388cdace7f72729bde40f0a2f30071b32
rohan-cogncise <rohana@cogncise.com>
2022-10-20 16:27:47 +0530

    added response format

---
+++ utils/responseformat.py
@@ -0,0 +1,19 @@
+from rest_framework import status
+
+
+def success_response(data=None,message=None):
+    content = {
+        'response_code':status.HTTP_200_OK,
+        'success':True,
+        'message':message,
+        'data':data,
+    }
+    return content
+
+def fail_response(data=None,code=status.HTTP_500_INTERNAL_SERVER_ERROR,message=None):
+    content = {
+        'response_code': code,
+        'success': False,
+        'message': message,
+        'data': data,
+    }
