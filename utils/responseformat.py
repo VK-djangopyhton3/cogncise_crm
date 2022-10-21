09cf5f5a650b0f1d06ae061b8f44630347a07bcd
rohan-cogncise <rohana@cogncise.com>
2022-10-21 09:37:16 +0530

    bug fix in responseformat.py file

--- utils/responseformat.py
+++ utils/responseformat.py
@@ -1,19 +1,21 @@
 from rest_framework import status
 
 
-def success_response(data=None,message=None):
+def success_response(data=None, message=None):
     content = {
-        'response_code':status.HTTP_200_OK,
-        'success':True,
-        'message':message,
-        'data':data,
+        'response_code': status.HTTP_200_OK,
+        'success': True,
+        'message': message,
+        'data': data,
     }
     return content
 
-def fail_response(data=None,code=status.HTTP_500_INTERNAL_SERVER_ERROR,message=None):
+
+def fail_response(data=None, message=None, code=status.HTTP_500_INTERNAL_SERVER_ERROR):
     content = {
         'response_code': code,
         'success': False,
         'message': message,
         'data': data,
     }
+    return content
