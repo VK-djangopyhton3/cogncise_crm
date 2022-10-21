043011685ecf20ccc50ca499fc0822e8019f9383
rohan-cogncise <rohana@cogncise.com>
2022-10-21 22:11:33 +0530

    removed redundant functions

--- apps/users/api/views.py
+++ apps/users/api/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import render
+from django.views.generic import UpdateView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -14,27 +14,11 @@ User = get_user_model()
 class UserViews(APIView):
     permission_classes = [IsAdmin]
 
-    def get(self, request):
-        pass
-
     def post(self, request):
-
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
+            return Response(success_response(serializer.data, 'New user added'))
+        return Response(fail_response(serializer.errors, 'New user could not be added'))
 
-            return Response(success_response(serializer.data,'New user added'))
 
-        return Response(fail_response(serializer.errors,'New user could not be added'))
-
-    def put(self, request):
-        pass
-
-    def delete(self, request):
-        pass
-
-    def get_queryset(self, request):
-        qs = User.objects.all()
-        if not request.user.is_staff:
-            qs = User.objects.filter(userrole__company__company_name=request.user.company.name)
-        return qs
