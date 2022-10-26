3e0cc4876098a1d4560bcf7395e7cf9d2bbd6144
rohan-cogncise <rohana@cogncise.com>
2022-10-26 08:57:28 +0530

    created table for update request

--- apps/company/models.py
+++ apps/company/models.py
@@ -1,12 +1,29 @@
+from django.conf import settings
 from django.db import models
+
 
 
 # Create your models here.
 class Companies(models.Model):
     company_name = models.CharField(max_length=255, null=True, blank=True, unique=True)
     company_address = models.CharField(max_length=255)
-    ABN = models.CharField(max_length=255,null=True, unique=True)
+    ABN = models.CharField(max_length=255, null=True, unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.company_name
 
+
+class CompanyUpdateRequests(models.Model):
+    company = models.ForeignKey(Companies, on_delete=models.CASCADE)
+    requested_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=255, null=True, blank=True)
+    company_address = models.CharField(max_length=255, null=True, blank=True)
+    ABN = models.CharField(max_length=255, null=True, blank=True)
+    is_approved = models.BooleanField(default=None)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    remarks = models.TextField()
+
+    def __str__(self):
+        return self.company
