6100c93e2de4a0c0b3d37dfa77a22c8375461734
rohan-cogncise <rohana@cogncise.com>
2022-11-02 10:44:26 +0530

    update sms consent

--- apps/customer/api/urls.py
+++ apps/customer/api/urls.py
@@ -4,6 +4,6 @@ from apps.customer.api import views
 
 urlpatterns = [
     path('add-customer/', views.add_customer, name='add-customer'),
-    path('search-customer', views.CustomerListSearch.as_view(), name='add-customer'),
-    path('update-sms-consent', views.sms_consent_update, name='add-customer')
+    path('search-customer', views.CustomerListSearch.as_view(), name='search-customer'),
+    path('update-sms-consent/', views.sms_consent_update, name='update-sms-consent')
 ]
