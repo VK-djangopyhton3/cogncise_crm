1cc12ae95199dbb223594eda1dff88d0d6f172ae
rohan-cogncise <rohana@cogncise.com>
2022-11-09 14:15:11 +0530

    Added tables for leads

--- apps/leads/models.py
+++ apps/leads/models.py
@@ -1,7 +1,36 @@
 from django.db import models
 
+from apps.company.models import Companies
+from apps.customer.models import CustomerInfo
+from apps.properties.models import Property
+from crm_backend import settings
+from utils.options import LEAD_STATUS
+
+
 # Create your models here.
 
-#     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-#     agency = models.ForeignKey(Companies, on_delete=models.CASCADE, null=True)
-#     created_on = models.DateTimeField(auto_now_add=True)
+class WorkType(models.Model):
+    work_type = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.work_type
+
+
+class Leads(models.Model):
+    customer = models.ForeignKey(CustomerInfo, on_delete=models.CASCADE)
+    property_address = models.ForeignKey(Property, on_delete=models.CASCADE)
+    agent = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    work_type = models.ForeignKey(WorkType, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    lead_status = models.CharField(max_length=50, choices=LEAD_STATUS)
+
+
+class LeadTransferHistory(models.Model):
+    lead = models.ForeignKey(Leads, on_delete=models.CASCADE)
+    agency = models.ForeignKey(Companies, on_delete=models.CASCADE)
+    assignee_number = models.IntegerField()
+    assigned_on = models.DateTimeField(auto_now=True)
+    is_current_assignee = models.BooleanField(default=True)
+
