a8fa7244686ddb367664c08becb001952228d91e
Anmol Kumar <anmolkumar.santophy@cogncise.com>
2022-12-03 18:21:47 +0530

    Add 'company' in Lead and Job model

--- crm_backend/apps/job/models.py
+++ crm_backend/apps/job/models.py
@@ -4,11 +4,13 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.contenttypes.fields import GenericRelation
 
 from shared.models import Address
+from company.models import Company
 from core.abstract_models import BasicInformation
 
 class Job(BasicInformation):
-    title = models.CharField( _("title"), max_length=100)
-    addresses = GenericRelation(Address, related_query_name='job')
+    title = models.CharField( _("title"),  max_length=100)
+    addresses = GenericRelation(Address,   related_query_name='job')
+    company   = models.ForeignKey(Company, related_name="job_company", on_delete=models.CASCADE, null=True, blank=True)
     
     def __str__(self):
         return f"{self.title}"
--- crm_backend/apps/lead/models.py
+++ crm_backend/apps/lead/models.py
@@ -3,6 +3,7 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.contenttypes.fields import GenericRelation
 
 from core.abstract_models import BaseModel, BasicInformation
+from company.models import Company
 from shared.models import Address
 
 User = get_user_model()
@@ -31,10 +32,11 @@ class LeadSource(BaseModel):
 
 class Lead(BasicInformation):
     addresses = GenericRelation(Address,      related_query_name='lead')
-    status    = models.ForeignKey(LeadStatus, related_name="status",   on_delete=models.CASCADE)
-    source    = models.ForeignKey(LeadSource, related_name="source",   on_delete=models.CASCADE)
-    owner     = models.ForeignKey(User,       related_name="owner",    on_delete=models.CASCADE)
-    customer  = models.ForeignKey(User,       related_name="customer", on_delete=models.CASCADE, null=True, blank=True)
+    status    = models.ForeignKey(LeadStatus, related_name="lead_status",   on_delete=models.CASCADE)
+    source    = models.ForeignKey(LeadSource, related_name="lead_source",   on_delete=models.CASCADE)
+    owner     = models.ForeignKey(User,       related_name="lead_owner",    on_delete=models.CASCADE)
+    customer  = models.ForeignKey(User,       related_name="lead_customer", on_delete=models.CASCADE, null=True, blank=True)
+    company   = models.ForeignKey(Company,    related_name="lead_company",  on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f"{self.first_name} {self.source}"
