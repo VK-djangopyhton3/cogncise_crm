c30a91ddab962824d791c87d5e29b31e9f600350
Vinod Kumar <vinodk@cogncise.com>
2022-12-04 20:11:38 +0530

    update wsgi and static

--- crm_backend/urls.py
+++ crm_backend/urls.py
@@ -63,3 +63,4 @@ urlpatterns += api_urls
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
