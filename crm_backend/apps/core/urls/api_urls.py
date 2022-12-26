0acd342449904c246ad2d07c4cea1b06e2020e69
Anmol Kumar <anmolkumar.santophy@cogncise.com>
2022-12-26 15:26:08 +0530

    resolve api path

--- crm_backend/apps/core/urls/api_urls.py
+++ crm_backend/apps/core/urls/api_urls.py
@@ -6,10 +6,10 @@ app_name='core'
 router = routers.DefaultRouter()
 
 urlpatterns = [
-    path('auth/login/', LoginAPIView.as_view(), name='auth_login'),
-    path('auth/logout/', LogoutAPIView.as_view(), name='auth_logout'),
-    path('auth/roles/', RoleListView.as_view(), name='roles'),
-    path('auth/', include(router.urls)),
-    path('auth/otp/login/', OTPLoginAPIView.as_view(), name='otp_login'),
-    path('auth/send-otp/', SendOTPView.as_view(), name='auth_send_otp'),
+    path('auth/login/',         LoginAPIView.as_view(),     name='auth_login'),
+    path('auth/logout/',        LogoutAPIView.as_view(),    name='auth_logout'),
+    path('auth/roles/',         RoleListView.as_view(),     name='roles'),
+    path('auth/',               include(router.urls)),
+    path('auth/otp/login/',     SendOTPView.as_view(),      name='auth_send_otp'),
+    path('auth/otp/verify/',    OTPLoginAPIView.as_view(),  name='otp_login'),
 ]
